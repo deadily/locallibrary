5ce68888239cf5942160b5bc009b0e222b85540c
dara <zuevadara04@gmail.com>
2025-11-11 18:48:31 +0700

    Initial commit

---
+++ locallibrary/catalog/admin.py
@@ -0,0 +1,47 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
+
+admin.site.register(Genre)
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+    fields = ('title', 'isbn', 'display_genre')
+    readonly_fields = ('display_genre',)
+    show_change_link = True
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+
+admin.site.register(Author, AuthorAdmin)
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'get_status_display_with_color', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
